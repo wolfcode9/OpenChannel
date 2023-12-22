3fa01bd2f98c47a80de42a144b7bf4d5520f9bbf
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-23 02:27:36 +0800

    Update py_czspp.py

--- py/py_czspp.py
+++ py/py_czspp.py
@@ -193,11 +193,12 @@ class Spider(Spider):
         }
         return result
     
-    '''
+    
     config = {
         "player": {},
         "filter": {}
     }
+    '''
     header = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
     }
