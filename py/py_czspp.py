a1885c70e1c84dc6882511a4c91f87af26e1537e
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-23 04:31:32 +0800

    Update py_czspp.py

--- py/py_czspp.py
+++ py/py_czspp.py
@@ -92,7 +92,7 @@ class Spider(Spider):
         node = root.xpath("//div[@class='dyxingq']")[0]
         pic = node.xpath(".//div[@class='dyimg fl']/img/@src")[0]
         title = node.xpath('.//h1/text()')[0]
-        remarks = node.xpath('.//li[contains(text(), "豆瓣")]/a')[0].text 
+        remarks = node.xpath('.//li[contains(text(), "上映")]/a')[0].text 
         year = node.xpath('.//li[contains(text(), "年份")]/a')[0].text
         area = node.xpath('.//li[contains(text(), "地区")]/a')[0].text
         typen = node.xpath('.//li[contains(text(), "类型")]/a')[0].text
@@ -106,7 +106,7 @@ class Spider(Spider):
             "type_name": typen,
             "vod_year": year,
             "vod_area": area,
-            "vod_remarks": ('豆瓣：' + remarks),
+            "vod_remarks": remarks,
             "vod_actor": actor,
             "vod_director": director,
             "vod_content": detail
