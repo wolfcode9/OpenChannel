fafa56854788984701ccc1ec0249a5a9d00ee0a4
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-25 20:00:20 +0800

    Update py_yingshi.py

--- py/py_yingshi.py
+++ py/py_yingshi.py
@@ -48,7 +48,7 @@ class Spider(Spider):
 	def homeVideoContent(self):
 		result = {}				
 		videos = []
-		limit = 20
+		limit = 35
 		url = f'{self.siteUrl}/ajax/data.html?mid=1&limit={limit}&by=score&order=desc'	
 		rsp = self.fetch(url) #rsp = self.fetch(self.siteUrl)
 		#root = self.html(rsp.text)
