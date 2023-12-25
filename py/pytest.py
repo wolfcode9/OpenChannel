004bff9c1642910973bf6c426fb0714739f237b1
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-25 23:50:23 +0800

    1212

--- py/pytest.py
+++ py/pytest.py
@@ -144,23 +144,7 @@ def filter(elements, key, name, index):
     return {"key": key, "name": name, "values": values}
 
 
-
-data = []
-cateManual = {
-        "電視劇": "1",
-        "電影": "2",
-        "綜藝": "3",
-        "動漫": "4",
-        "記錄片": "5"
-}	
-    
-for k in cateManual:
-    data.append({cateManual[k]: job(cateManual[k])})
-
-g = json.dumps(data, ensure_ascii=False) #, indent=2)
-#g = ', '.join(json.dumps(item, ensure_ascii=False) for item in data)
-with open('a.txt','w',encoding='utf8') as f:
-    f.write(g)
+print(job(3))
 
 
 
