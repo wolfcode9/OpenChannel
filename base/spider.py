18fbd6535122f7e7bcc47deecbfbbee08cd9df8a
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-27 01:12:49 +0800

    Update spider.py

--- base/spider.py
+++ base/spider.py
@@ -38,19 +38,21 @@ class Spider(metaclass=ABCMeta):
     def getName(self):pass
     def getDependence(self):
         return []
+    
     def regStr(self,src,reg,group=1):
         m = re.search(reg, src)
         src = ''
         if m :
             src = m.group(group)
         return src
+    
     def str2json(self,str):
-
         return json.loads(str)
+    
     def cleanText(self,src):
         clean = re.sub('[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF]', '', src)
         return clean
-    
+        
     def fetch(self, url, headers={}, cookies={}, params={}):        
         rsp = requests.get(url,headers=headers,cookies=cookies,params=params)
         rsp.encoding='utf-8'
