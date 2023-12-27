55aa011f6b16c598d7e405d1ed700e271dcb912c
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-28 01:45:33 +0800

    Update py_douban.py

--- py/py_douban.py
+++ py/py_douban.py
@@ -8,22 +8,22 @@ import json
 class Spider(Spider):
 
 	def getName(self):
-		return "熱門影片推薦"
+		return "豆瓣薦片"
 	
 	def init(self,extend=""):
 		pass	
 	
-	#主頁
 	def homeContent(self,filter):
 		result = {}
 		return result
 	
-	#推薦
+	#推薦頁面
 	def homeVideoContent(self):
 		result = {}
 		header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-		#電視劇 https://movie.douban.com/j/search_subjects?type=tv&tag=%E7%83%AD%E9%97%A8&page_limit=50&page_start=0
-		rsp = self.fetch('https://movie.douban.com/j/search_subjects?tag=热门&page_limit=50&page_start=0',headers=header)	
+		#電視劇 https://movie.douban.com/j/search_subjects?type=tv&tag=热门&page_limit=50
+		#電影 https://movie.douban.com/j/search_subjects?type=movie&tag=热门&page_limit=50
+		rsp = self.fetch('https://movie.douban.com/j/search_subjects?type=tv&tag=热门&page_limit=50',headers=header)	
 		vData = json.loads(rsp.text)
 		vod = []
 		for v in vData['subjects']:
@@ -35,35 +35,28 @@ class Spider(Spider):
 		result = {"list":vod}
 		return result
 	
-	#分類
 	def categoryContent(self,tid,pg,filter,extend):
 		result = {}
 		return result
 	
-	#詳情
 	def detailContent(self,array):
 		result = {}
 		return result
 	
-	#搜索
 	def searchContent(self,key,quick):
 		result = {}
 		return result
 	
-	#播放
 	def playerContent(self,flag,id,vipFlags):
 		result = {}
 		return result
 	
-	#視頻格式
 	def isVideoFormat(self,url):
 		pass
 	
-	#視頻檢測
 	def manualVideoCheck(self):
 		pass
 	
-	#本地代理
 	def localProxy(self,param):
 		action = {
 			'url':'',
