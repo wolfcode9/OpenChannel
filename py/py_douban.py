5b3f1425db2060a55cf39e20293f74e25caef245
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-28 20:45:58 +0800

    Update py_douban.py

--- py/py_douban.py
+++ py/py_douban.py
@@ -4,10 +4,15 @@ import sys
 sys.path.append('..') 
 from base.spider import Spider
 import json
+import concurrent.futures
+import requests
 
 class Spider(Spider):
-	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-
+	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}	
+	url_movie = "https://movie.douban.com/j/search_subjects?type=movie&tag=热门&page_limit=50&page_start=0"
+	url_tv = "https://movie.douban.com/j/search_subjects?type=tv&tag=热门&page_limit=50&page_start=0"	
+	
+	
 	def getName(self):
 		return "豆瓣薦片"
 	
@@ -27,15 +32,25 @@ class Spider(Spider):
 		return result	
 	
 	def homeVideoContent(self):
-		result = {}		
+		result = {}
+		limit = 10
 		#電視劇 https://movie.douban.com/j/search_subjects?type=tv&tag=热门&page_limit=50&page_start=0
-		#電影 https://movie.douban.com/j/search_subjects?type=movie&tag=热门&page_limit=50&page_start=0	
-		
-		result = {"list" : self.get_douban_hot("tv",50,0)}
-		return result
+		#電影 https://movie.douban.com/j/search_subjects?type=movie&tag=热门&page_limit=50&page_start=0
+
+		with concurrent.futures.ThreadPoolExecutor() as executor:					
+			json_movie = executor.submit(self.fetch_vodData,self.douban_url('movie',limit,0)).result()
+			json_tv = executor.submit(self.fetch_vodData,self.douban_url('tv',limit,0)).result()
+			vod = json_movie + json_tv
+			result = {'list': vod}
+		return result 
 	
-	def categoryContent(self,tid,pg,filter,extend):		
-		result = {"list" : self.get_douban_hot(tid,50,pg)}
+	def categoryContent(self,tid,pg,filter,extend):
+		result = {}
+		limit = 50
+		result['list'] = self.fetch_vodData(self.douban_url(tid,limit,pg))
+		result['page'] = limit
+		result['limit'] = 50
+		result['total'] = 500
 		return result
 	
 	def detailContent(self,array):
@@ -66,19 +81,18 @@ class Spider(Spider):
 		}
 		return [200, "video/MP2T", action, ""]
 	
-	def get_douban_hot(self,typeid,limit, pg):
-		rsp = self.fetch(f'https://movie.douban.com/j/search_subjects?type={typeid}&tag=热门&page_limit={limit}&page_start={pg}',headers=self.header)	
-		jsonData = json.loads(rsp.text)
-		vod = []		
+	def douban_url(self,typeid,limit, pg):
+		return f'https://movie.douban.com/j/search_subjects?type={typeid}&tag=热门&page_limit={limit}&page_start={pg}'
+
+	def fetch_vodData(self,url):
+		vod = []
+		jsonData = requests.get(url=url,headers = self.header).json()		
 		for v in jsonData['subjects']:
 			remarks = v['episodes_info'] or v['rate']
-			vod.append({
-				"vod_id": None,
+			vod.append({				
+				"vod_id": '',
 				"vod_name": v['title'],
 				"vod_pic": v['cover'],
 				"vod_remarks": remarks
 			})
 		return vod
-		
-
-	
