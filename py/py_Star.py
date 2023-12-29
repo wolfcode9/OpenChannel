8a852b66a9af2d7bb75b0eb91c9c46fc7ebb26ce
大老虎咬毛巾 <hsuan104@gmail.com>
2023-12-29 23:49:55 +0800

    Update py_Star.py

--- py/py_Star.py
+++ py/py_Star.py
@@ -4,39 +4,48 @@ import sys
 sys.path.append('..') 
 from base.spider import Spider
 import requests
-import re
-from lxml import html
+import urllib.parse
+import json
 
 class Spider(Spider):
 	apiUrl = "https://aws.ulivetv.net/v3/web/api/filter"
 	siteUrl = "https://www.histar.tv/"
 	detail = siteUrl + "vod/detail/"
 	data = "_next/data/"
+	ver = ""
 	header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Cookie": "userIP=127.0.0.1; aws-waf-token=",
         "Referer": siteUrl
     }
 	classes = [
-		{"type_id": "movie", "type_name": "電影"}, 
-		{"type_id": "drama", "type_name": "電視劇"},
-		{"type_id": "variety", "type_name": "綜藝"}, 
-		{"type_id": "animation", "type_name": "動漫"},
-		{"type_id": "documentary", "type_name": "記錄片" }
-	]
+		{"type_id": "movie", "type_name": "电影"}, 
+		{"type_id": "drama", "type_name": "电视剧"},
+		{"type_id": "variety", "type_name": "综艺"}, 
+		{"type_id": "animation", "type_name": "动漫"},
+		{"type_id": "documentary", "type_name": "纪录片"}
+	]	
 
 	def getName(self):
 		return "星視界"
 	
-	def init(self,extend=""):		
+	def init(self,extend=""):
 		self.extend = extend
+		self.ver = self.getVer()
 	
 	def homeContent(self,filter):
 		result = {}	
+		f = []
 		result['class'] = self.classes
-		if self.extend:
-			result['filters'] =  self.fetch(self.extend).json()
+		for cls in self.classes:
+			url = self.siteUrl + cls["type_id"] + "/all/all/all"
+			rsp = self.fetch(url,headers=self.header)
+			tree = self.html(rsp.text)			
+			script = self.str2json(self.xpText(tree,'//script[@id="__NEXT_DATA__"]/text()'))
+			f.append(script["props"]["pageProps"]["filterCondition"]["label"])
+		result['filters'] =  f #self.fetch(self.extend).json()
 		return result
+		#if self.extend:
 	
 	def homeVideoContent(self):		
 		vod = []
@@ -55,33 +64,31 @@ class Spider(Spider):
 				})			
 		return	{"list":vod}
 	
-	def categoryContent(self,tid,pg,filter,extend):		
-		result = {}
+	def categoryContent(self,tid,pg,filter,extend):
+		result = {}		
+		cnName = next(cls["type_name"] for cls in self.classes if cls["type_id"] == tid)		
+		query = {
+    		"chName":cnName,
+    		"pageSize":16,
+    		"page":pg,
+            "year": extend.get("year", ""),
+            "type": extend.get("type", ""),
+            "area": extend.get("area", "")
+		}	
+		jsonData = requests.post(url=self.apiUrl,json=query,headers=self.header).json()		 
 		vod = []
-		params = {
-			"ChName": "电视直播",
-			"PageSize": 16,
-			"type": extend.get("type", ""),	
-			"year": extend.get("year", ""),
-			"area": extend.get("area", "")
-		}
-		rsp = requests.get(url=self.apiUrl,params=params,hasattr=self.header)
-		print(rsp.text)
-		'''
-		jsonData = rsp.json()		
 		for v in jsonData['data']['list']:
 			vod.append({
 				"vod_id": v['id'],
 				"vod_name": v['name'],
-				"vod_pic": v['picture'],
-				"vod_remarks": v['remarks']
-			})
+				"vod_pic": v['img'],
+				"vod_remarks": v['countStr']
+        })
 		result['list'] = vod
 		result['page'] = pg
-		result['pagecount'] = jsonData['page']['pageCount']
+		result['pagecount'] = jsonData['data']['pageSize']
 		result['limit'] = 35
-		result['total'] = jsonData['page']['total']
-		'''
+		result['total'] = jsonData['data']['total']		
 		return result 
 	
 	def detailContent(self,array):
@@ -110,24 +117,18 @@ class Spider(Spider):
 		return {"list": vod}
 	 
 	def searchContent(self,key,quick):
-		'''
-		#https://m.mubai.link/search?search=我知道我爱你
-		result = {}
-		url = f"{self.siteUrl}/api/searchFilm?keyword={key}'
-		rsp = self.fetch(url)		
-		vod = []
-		jsonData = rsp.json()
-		jsonData = jsonData['data']['list']
+		url = f"{self.siteUrl}{self.data}{self.ver}/search.json?word={urllib.parse.quote(key)}"
+		jsonData = self.fetch(url,headers=self.header).json()
+		vod = []		
+		jsonData = jsonData['pageProps']['initList']
 		for v in jsonData:
 			vod.append({
 				"vod_id": v['id'],
 				"vod_name": v['name'],
-				"vod_pic": v['picture'],
-				"vod_remarks": v['remarks']
-			})      
-		result['list'] = vod
-		'''	
-		return {}
+				"vod_pic": v['picurl'],
+				"vod_remarks": v['countStr']
+			})			
+		return {"list" : vod}
 	
 	def playerContent(self,flag,id,vipFlags):
 		result = {
@@ -153,3 +154,11 @@ class Spider(Spider):
 			'after':''
 		}
 		return [200, "video/MP2T", action, ""]
+	
+	def getVer(self):    
+		rsp = self.fetch(self.siteUrl,headers=self.header)
+		tree = self.html(rsp.text)			
+		ver = tree.xpath('//script[contains(@src, "_buildManifest.js")]/@src')[0]
+		ver = ver.split("/")[3]
+		return ver
+		
