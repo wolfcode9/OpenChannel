7027e3566434a2e09a48cdfd97ba815532847181
大老虎咬毛巾 <86637213+wolfcode9@users.noreply.github.com>
2024-01-05 22:49:51 +0800

    Add files via upload

--- py/local.py
+++ py/local.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 #!/usr/bin/python
 from importlib.machinery import SourceFileLoader
-import os
 
 def loadFromDisk(fileName):
     name = fileName.split('/')[-1].split('.')[0]
@@ -15,24 +14,13 @@ def run(pyname,extend):
     #Q = sp.homeVideoContent() # 主页视频
     #Q = sp.detailContent(["2200"]) # 详情
     #Q = sp.categoryContent('dsj','1',False,{}) # 分类
-    Q = sp.searchContent("陌上人如",False) # 搜索 
+    Q = sp.searchContent("萌妹子",False) # 搜索 
     #Q = sp.playerContent("","bXZfMjIwMC1ubV8x",{}) # 播放
-    print(Q['list'][0])
+    print(Q)
                         
 if __name__ == '__main__':
     pyname = "py_app.py"
-    extend = "https://kuaikan-api.com/api.php/provide/vod/from/kuaikan"
+    extend = "https://www.gdlsp.com/api/xml.php"
     run(pyname,extend=extend)
 
 
-#飛速V	https://www.feisuzyapi.com/api.php/provide/vod/
-#速播V	https://subocaiji.com/api.php/provide/vod/at/json
-#量子V	https://cj.lziapi.com/api.php/provide/vod/
-#快看	https://kuaikan-api.com/api.php/provide/vod/from/kuaikan
-#非凡V	https://cj.ffzyapi.com/api.php/provide/vod/
-#暴風	https://bfzyapi.com/api.php/provide/vod
-# ?ac=videolist&search?text=dota&pg=1
-# ?ac=videolist&zm=dota&page=1
-# ?ac=videolist&wd=dota&page=1
-# /list?wd=dota&page=1
-# ?wd=dota&page=1
