de73ccae9a4bb3d2bf52299c31923bfa03636f19
大老虎咬毛巾 <86637213+wolfcode9@users.noreply.github.com>
2024-01-05 22:49:02 +0800

    Add files via upload

--- py/py_app.py
+++ py/py_app.py
@@ -3,12 +3,11 @@
 import sys
 sys.path.append('..')
 from base.spider import Spider
-import re
 
 class Spider(Spider):    
 
     def getName(self):
-        return "影視App"
+        return "Wolf"
     
     def init(self,extend=""):
         self.siteUrl = extend
@@ -18,9 +17,7 @@ class Spider(Spider):
         rsp = self.fetch(f'{self.siteUrl}?ac=list&h=1')
         if rsp:            
             result = {'class': rsp.json()['class']}
-        return result    
-        #if(filter):
-        #    result['filters'] = self.config['filter']
+        return result
 
     def homeVideoContent(self):
         rsp = self.fetch(f'{self.siteUrl}?ac=videolist&h=24')
@@ -46,33 +43,12 @@ class Spider(Spider):
         return result
 
     def searchContent(self,key,quick):
-        '''
-
-        H = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Host": "kuaikan-api.com",
-            "Referer" : "https://kuaikan-api.com"
-        }
-        '''
-        result = {}        
-        patterns = [            
-            {'keyword': {'ffzyapi','feisuzyapi','subocaiji','lziapi'}, 'param': '?ac=videolist&wd={key}&page=1'},
-            #{'keyword': {}, 'param': '/list?wd={key}&page=1'},            
-            #{'keyword': {}, 'param': '?wd={key}&page=1'},
-            #{'keyword': {'kuaikan',}, 'param': '?ac=videolist&search?text={key}&pg=1'},
-            #{'keyword': {'bfzyapi','kuaikan'}, 'param': '?ac=videolist&zm={key}&page=1'},
-        ]
-        URL = ""
-        for pattern in patterns:
-            for keyword in pattern['keyword']:
-                if re.search(keyword,self.siteUrl):
-                    URL = self.siteUrl + pattern['param'].format(key=key)
-                    break
-        if URL:
-            rsp = self.fetch(URL)
-            if rsp:
-                result = rsp.json()
-        return result
+        result = {}
+        url = f'{self.siteUrl}?ac=videolist&wd={key}&page=1'
+        rsp = self.fetch(url)
+        if rsp:
+            result = rsp.json()
+        return result    
     
     def playerContent(self,flag,id,vipFlags):
         result = {}
